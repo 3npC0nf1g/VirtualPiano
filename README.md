b795f79d88a9bc3c1f463f51970dba68551a5b04
3npC0nfig <teumayouomo113@gmail.com>
2022-08-20 06:17:29 +0200

    project(all): Virtual Piano version 1.0

---
+++ README.md
@@ -0,0 +1,12 @@
+![](img.png "img.png")
+
+# Objectif of this project
+
+* Create a virtual piano which can be use with some keys of our keyboard.
+
+# How to use the code
+
+* you just have to open the index.html file with a browser.
+* after that used the keys of your keyboard corresponding to those specified in the web piano to make some songs and take pleasure.
+
+# ReadMe to complete ...
---
+++ main.js
@@ -0,0 +1,54 @@
+document.addEventListener("keydown", function (event) {
+    switch (event.code) {
+        case "KeyA":
+            let audioA = new Audio("TouchSounds/A.mp3");
+            audioA.play();
+            break;
+        case "KeyS":
+            let audioS = new Audio("TouchSounds/S.mp3");
+            audioS.play();
+            break;
+        case "KeyD":
+            let audioD = new Audio("TouchSounds/D.mp3");
+            audioD.play();
+            break;
+        case "KeyF":
+            let audioF = new Audio("TouchSounds/F.mp3");
+            audioF.play();
+            break;
+        case "KeyG":
+            let audioG = new Audio("TouchSounds/G.mp3");
+            audioG.play();
+            break;
+        case"KeyH":
+            let audioH = new Audio("TouchSounds/H.mp3");
+            audioH.play();
+            break;
+        case"KeyJ":
+            let audioJ = new Audio("TouchSounds/J.mp3");
+            audioJ.play();
+            break;
+        case "KeyW":
+            let audioW = new Audio("TouchSounds/W.mp3");
+            audioW.play();
+            break;
+        case"KeyE":
+            let audioE = new Audio("TouchSounds/E.mp3");
+            audioE.play();
+            break;
+        case"KeyT":
+            let audioT = new Audio("TouchSounds/T.mp3");
+            audioT.play();
+            break;
+        case"KeyY":
+            let audioY= new Audio("TouchSounds/Y.mp3");
+            audioY.play();
+            break;
+        case"KeyU":
+            let audioU = new Audio("TouchSounds/U.mp3");
+            audioU.play();
+            break;
+        default:
+            break;
+    }
+});
